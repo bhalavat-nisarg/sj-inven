cb430e2e9793d8fbb473fe571017b98e5f2e722e
Nisarg Bhalavat <31996250+bhalavat-nisarg@users.noreply.github.com>
2021-09-19 02:52:02 +0530

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sj-inven',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
